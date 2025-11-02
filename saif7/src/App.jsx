8ac2322385eceede3906c45310b9ee3b3b520c55
Shaharia66 <gssaif.tm@gmail.com>
2025-11-02 13:13:09 +0600

    day1

---
+++ saif7/src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+function App() {
+
+  const url="https://dummyjson.com/user"
+  
+  const [usersData,setUsersData]=useState([]);
+  useEffect(()=>{
+    getData()
+  },[])
+  async function getData(){
+    let response=await fetch(url);
+    response=await response.json();
+    // console.log(response);
+    setUsersData(response.users)
+  }
+  console.log(usersData);
+  return (
+    <>
+    <h1>Here starts fatching api.</h1>
+    <ul className="list list_head">
+        <li>First Name </li>
+        <li>Last Name</li>
+        <li>Age</li>
+      </ul>
+    {
+      usersData.map((p)=>(  
+      <ul className="list">
+        <li>{p.firstName}</li>
+        <li>{p.lastName}</li>
+        <li>{p.age}</li>
+      </ul>
+      ))
+    }
+    </>
+  )
+}
+
+export default App
